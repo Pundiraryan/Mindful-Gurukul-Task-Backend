8fb6ca6a2870afd3f0e0be7574cbbe785285aa71
Aryan Pundir <95073218+Pundiraryan@users.noreply.github.com>
2024-01-04 11:26:44 +0530

    Update index.js

--- index.js
+++ index.js
@@ -8,13 +8,15 @@ dotenv.config();
 connectToMongo()
   .then(() => {
     app.listen(process.env.PORT, () => {
-      console.log(`App listening on port ${process.env.PORT}`);
+      console.log(`App listening on port http://localhost:${process.env.PORT}`);
     });
   })
   .catch((error) => {
     console.error('Error starting the app:', error);
   });
-
+  app.get("/",(req,res)=>{
+    res.send("home page is this");
+  })
   app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(cors());
